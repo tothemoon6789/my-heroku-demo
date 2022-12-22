f7b759e26cb2732c54c048381eb70395e5e9c5b8
tothemoon6789 <pnlinh6789@gmail.com>
2022-12-23 01:01:41 +0700

    hello wolrd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import './App.css';
-import Developer from './developer';
-import app from './developer/firebase';
-import { getDatabase, ref, onValue } from 'firebase/database';
-function App() {
-  const database = getDatabase(app);
-  const starCountRef = ref(database, 'developer/');
+import BasicExample from './component/example'
 
+export default function App() {
   return (
-    <>
-    {console.log("RENDER APP TODY")}
-      {onValue(starCountRef, (snapshot) => {
-        const data = snapshot.val()
-        console.log("RENDER APP");
-        console.log(data);
-      })}
-      <Developer />
-    </>
-  );
+   <BasicExample/>
+  )
 }
-
-export default App;
